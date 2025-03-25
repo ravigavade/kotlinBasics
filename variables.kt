b5ed808857bdaa3f9d10aeabb37fd42dc99f14a0
Ravi Gavade <gavaderavi4@gmail.com>
2025-03-25 14:07:13 -0400

    variables

--- variables.kt
+++ variables.kt
@@ -1,21 +1,27 @@
 
 fun main() {
-    val y=8
-    val z:Double=8767869768768.3
-    val x:Float=5.9f
-    println(y)
-    println(x)
-    println(z)
+//    val y=8
+//    val z:Double=8767869768768.3
+//    val x:Float=5.9f
+//    println(y)
+//    println(x)
+//    println(z)
+//
+//    val q:Boolean=true
+//    println(q)
+//
+//    println("Supp boi")
+//    val name="csaim"
+//    println(name)
+//
+//    var ans:Double=9.4
+//    ans=x+z
+//    println(ans)
 
-    val q:Boolean=true
-    println(q)
-
-    println("Supp boi")
     val name="csaim"
-    println(name)
+    val game="valorant"
 
-    var ans:Double=9.4
-    ans=x+z
+    val ans= "my name is $name and i like playing $game"
+
     println(ans)
-
 }
