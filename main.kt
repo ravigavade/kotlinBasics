5989bf353a013a22500b97e9c4d49263c621d218
Ravi Gavade <gavaderavi4@gmail.com>
2025-03-25 14:04:19 -0400

    first commit

---
+++ main.kt
@@ -0,0 +1,6 @@
+
+fun main(){
+    println("hello world")//prints and then cursor goes to next line
+    print("csaim")
+
+}
---
+++ variables.kt
@@ -0,0 +1,21 @@
+
+fun main() {
+    val y=8
+    val z:Double=8767869768768.3
+    val x:Float=5.9f
+    println(y)
+    println(x)
+    println(z)
+
+    val q:Boolean=true
+    println(q)
+
+    println("Supp boi")
+    val name="csaim"
+    println(name)
+
+    var ans:Double=9.4
+    ans=x+z
+    println(ans)
+
+}
