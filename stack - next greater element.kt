09455838de3b0d2034eb68640a532c03dd571059
ravi <gavaderavi4@gmail.com>
2025-05-05 19:49:50 -0400

    stack - next greater element

---
+++ stack - next greater element.kt
@@ -0,0 +1,27 @@
+
+
+fun main(){
+
+    val nums = intArrayOf(6,8,0,1,9,1)
+    val stack = ArrayDeque<Int>()
+    val result = IntArray(nums.size)
+
+    for (i in nums.lastIndex downTo 0){
+
+        while (stack.isNotEmpty() && stack.last() < nums[i]){
+            stack.removeLast()
+        }
+
+        if (stack.isNotEmpty() && stack.last() > nums[i]){
+            result[i] = stack.last()
+        }
+
+        stack.addLast(nums[i])
+    }
+
+    for (n in result) print(n)
+
+
+
+
+}
